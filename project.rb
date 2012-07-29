58040ddb2111ddb5fc0366e4232583cf132a7316
Paul Sherwood <paul.sherwood@codethink.co.uk>
2012-07-29 18:10:50 +0100

    more hacking - and a run button

--- project.rb
+++ project.rb
@@ -1,20 +1,24 @@
 # feedback control response simulation
-Shoes.app :width => 1500, :height => 1000 do
-
-  @day = para "DAY"
-  @gap = para "GAP"
+Shoes.app :width => 1500, :height => 1000, :title => "Project Control Simulator" do
+  @push = button "Run"
   @spec = oval :fill => red, :left => 0, :top => 950, :radius => 5, :speed => 15, :name => 'Spec'
-  @team = oval :fill => green, :left => 0, :top => 1000, :radius => 5, :speed => 6, :name => 'Team'
-  @loc = 0
-  @teamsize = 1
-  @sprint = 70
+  @work = oval :fill => green, :left => 0, :top => 1000, :radius => 5, :speed => 6, :name => 'Work'
+  @teamsize = 5
+  lag = 5
+  uncertainty = 100
 
-  lag = 10
-  error = 0
-  l = 5
-  p = 0.1
-  d = 0.2
-  oldgap = 0
+  @sprint = 40
+  iterations = 10
+
+  capacity = @sprint * @teamsize
+  @scale = 3
+  para "SPRINT: #{@sprint} days "
+  para "LAG: #{lag} days "
+  para "UNCERTAINTY: #{uncertainty}% "
+  @team = para "TEAMSIZE: #{@teamsize} "
+  @day = para "DAY: "
+  @gap = para "GAP: "
+
   gaparray = Array.new(600) {|i| 0 }
 
   def active day
@@ -27,24 +31,31 @@ Shoes.app :width => 1500, :height => 1000 do
 
   def trail id
     stroke id.fill
-    oval(id.left, id.top + 5,3,3)
+    oval(id.left, id.top + 5,@scale, @scale)
   end
 
-  a = animate(25) do |day|
-    gap = @spec.top - @team.top + rand(error)
-    gaparray[day + lag] = gap
-    @spec.move day * 3, @spec.top
-    if (day % @sprint) == 0 and (@spec.top > 100)
-      @spec.top = @spec.top - rand(@sprint) * 2
+  @push.click {
+    a = animate(50) do |day|
+    a.stop if day >= iterations * @sprint - 1
+    gap = @spec.top - @work.top
+    error = uncertainty / 100 * (gap/2 - rand(gap.to_i))
+    gaparray[day + lag] = gap + error
+    @spec.move day * @scale, @spec.top
+    if (day % @sprint) == 0 and (@spec.top > 120)
+      step = rand(@sprint) * @scale
+      @spec.top = @spec.top - step
     end
 
-    @team.move day * 3, @team.top + gaparray[day] * p * active(day) * @teamsize
-    trail(@spec)
-    trail(@team)
+    workdone = active(day) * @teamsize * gaparray[day] / @sprint
 
-    @day.replace "DAY #{day}"
-    @gap.replace "GAP #{gap}"
-    (a.stop) if day > 400
+    @work.move day * @scale, (@work.top + workdone)
+    trail(@spec)
+    trail(@work)
+
+    @day.replace "DAY: #{day + 1}"
+    @gap.replace "GAP: #{gap} #{error.to_i}"
+    @team.replace "TEAMSIZE: #{@teamsize}"
     oldgap = gap
   end
+  }
 end
