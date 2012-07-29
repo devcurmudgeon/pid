bf04552a0366e24f9142a91435bf57f66893aa3b
Paul Sherwood <paul.sherwood@codethink.co.uk>
2012-07-29 22:00:11 +0100

    popup sim params

--- project.rb
+++ project.rb
@@ -1,14 +1,14 @@
 # feedback control response simulation
 Shoes.app :width => 1500, :height => 1000, :title => "Project Control Simulator" do
   @push = button "Run"
-  @teamsize = 5
+  teamsize = 5
   lag = 1
   uncertainty = 0
 
-  @sprint = 20
-  @iterations = 10
+  sprint = 20
+  iterations = 10
 
-  capacity = @sprint * @teamsize
+  capacity = sprint * teamsize
   @scale = 3
 
   gaparray = Array.new(600) {|i| 0 }
@@ -26,38 +26,31 @@ Shoes.app :width => 1500, :height => 1000, :title => "Project Control Simulator"
     oval(id.left, id.top + 5,@scale, @scale)
   end
 
-  @push.click  {
+  @push.click  { 
     @anim.stop if @anim
     background white
 
     @spec = oval :fill => red, :left => 0, :top => 950, :radius => 5, :speed => 15, :name => 'Spec'
     @work = oval :fill => green, :left => 0, :top => 1000, :radius => 5, :speed => 6, :name => 'Work'
-    @a = para "SPRINT: #{@sprint} days "
-    @b = para "LAG: #{lag} days "
-    @c = para "UNCERTAINTY: #{uncertainty}% "
-    @team = para "TEAMSIZE: #{@teamsize} "
-    @day = para "DAY: "
-    @gap = para "GAP: "
+    window :height => 20 do
+      para "SPRINT: #{sprint} days LAG: #{lag} days UNCERTAINTY: #{uncertainty}% TEAMSIZE: #{teamsize} "
+    end
     @anim = animate(50) do |day|
-      @anim.stop if day >= @iterations * @sprint - 1
+      @anim.stop if day >= iterations * sprint - 1
       gap = @spec.top - @work.top
       error = uncertainty / 100 * (gap/2 - rand(gap.to_i))
       gaparray[day + lag] = gap + error
       @spec.move day * @scale, @spec.top
-      if (day % @sprint) == 0 and (@spec.top > 120)
-        step = rand(@sprint) * @scale
+      if (day % sprint) == 0 and (@spec.top > 120)
+        step = rand(sprint) * @scale
         @spec.top = @spec.top - step
       end
 
-      workdone = active(day) * @teamsize * gaparray[day] / @sprint
+      workdone = active(day) * teamsize * gaparray[day] / sprint
 
       @work.move day * @scale, (@work.top + workdone)
       trail(@spec)
       trail(@work)
-      oldgap = gap
-      @day.replace "DAY: #{day + 1}"
-      @gap.replace "GAP: #{gap} #{error.to_i}"
-      @team.replace "TEAMSIZE: #{@teamsize}"
     end
   }
 end
